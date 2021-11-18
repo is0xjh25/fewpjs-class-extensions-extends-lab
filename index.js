f4567d2fa47b1a8ed78560c3dca565f63049659d
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2021-11-18 21:33:50 +0800

    Update: Test passed

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+class Polygon {
+	constructor(arr) {
+		this.arr = arr;
+	}
+
+	get countSides() {
+		return this.arr.length;
+	}
+
+	get perimeter() {
+		return this.arr.reduce((pre, next) => pre + next);
+	}
+}
+
+class Triangle extends Polygon {
+	get isValid() {
+		if (this.countSides != 3) return false;
+		if (this.arr[0] + this.arr[1] <= this.arr[2] || 
+			this.arr[0] + this.arr[2] <= this.arr[1] || 
+			this.arr[1] + this.arr[2] <= this.arr[0]) {
+        	return false;
+		} else {
+        	return true;
+		} 
+	}
+}
+
+class Square extends Polygon {
+	get isValid() {
+		if (this.countSides != 4) return false;
+		return (this.arr.every((ele) => {
+			return ele === this.perimeter / 4;
+			})
+		)
+	}
+
+	get area() {
+		return this.arr[0] * this.arr[0];
+	}
+}
